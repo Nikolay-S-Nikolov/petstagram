0f4154a1e8168cab674232b54286e40763035b6b
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2024-10-06 13:23:27 +0300

    Workshop files updated

--- petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/pets/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from petstagram.pets.views import create_pet, details_pet, edit_pet, delete_pet
+from petstagram.pets.views import details_pet, edit_pet, delete_pet, PetCreateView
 
 urlpatterns = (
-    path('create/', create_pet, name='create pet'),
-    path('<str:username>/pet/<slug:pet_slug>/', include([
+    path('create/', PetCreateView.as_view(), name='create pet'),
+    path('<str:username>/pet/<slug:slug>/', include([
         path('', details_pet, name='details pet'),
         path('edit/', edit_pet, name='edit pet'),
         path('delete/', delete_pet, name='delete pet'),
--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -1,33 +1,39 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import generic as views
 
 from petstagram.pets.forms import PetCreateForm, PetEditForm, PetDeleteForm
 from petstagram.pets.models import Pet
 
 
-def create_pet(request):
-    pet_form = PetCreateForm(request.POST or None)
-    if request.method == 'POST' and pet_form.is_valid():
-        created_pet = pet_form.save()
-        return redirect('details pet', username="Nikolay", pet_slug=created_pet.slug)
-    context = {
-        "pet_form": pet_form
-    }
-    return render(request, "pets/create_pet.html", context)
+class PetCreateView(views.CreateView):
+    # model = Pet # - it is not needed as form_class is used
+    form_class = PetCreateForm
+    template_name = "pets/create_pet.html"
+
+    def get_success_url(self):
+        return reverse(
+            'details pet',
+            kwargs={
+                "username": "Nikolay",
+                "slug": self.object.slug
+            })
 
 
-def details_pet(request, username, pet_slug):
+def details_pet(request, username, slug):
     context = {
-        "pet": Pet.objects.get(slug=pet_slug)
+        "pet": Pet.objects.get(slug=slug),
+        "username": username
     }
     return render(request, "pets/details_pet.html", context)
 
 
-def edit_pet(request, username, pet_slug):
-    pet_to_edit = Pet.objects.get(slug=pet_slug)
+def edit_pet(request, username, slug):
+    pet_to_edit = Pet.objects.get(slug=slug)
     pet_form = PetEditForm(request.POST or None, instance=pet_to_edit)
     if request.method == 'POST' and pet_form.is_valid():
         edited_pet = pet_form.save()
-        return redirect('details pet', username="Nikolay", pet_slug=edited_pet.slug)
+        return redirect('details pet', username="Nikolay", slug=edited_pet.slug)
     context = {
         "pet_form": pet_form,
         "username": username,
@@ -36,9 +42,8 @@ def edit_pet(request, username, pet_slug):
     return render(request, "pets/edit_pet.html", context)
 
 
-def delete_pet(request, username, pet_slug):
-
-    pet_to_delete = Pet.objects.get(slug=pet_slug)
+def delete_pet(request, username, slug):
+    pet_to_delete = Pet.objects.get(slug=slug)
     pet_form = PetDeleteForm(request.POST or None, instance=pet_to_delete)
     if request.method == 'POST' and pet_form.is_valid():
         pet_form.save()
@@ -48,4 +53,4 @@ def delete_pet(request, username, pet_slug):
         "username": username,
         "pet": pet_to_delete,
     }
-    return render(request, "pets/delete_pet.html",context)
+    return render(request, "pets/delete_pet.html", context)
